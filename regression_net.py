f62b9fcf00d12460320555576a0d4278d5989121
Shah Ismail <sim4life@yahoo.com>
2017-03-02 21:55:21 +0000

    Rev 02: normalized inputs

--- regression_net.py
+++ regression_net.py
@@ -35,6 +35,6 @@ model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
 
 model.save('model.h5')
