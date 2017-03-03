f63668329946d0d17c023bbc37565a76e106225d
Shah Ismail <sim4life@yahoo.com>
2017-03-03 21:16:40 +0000

    nv_net modified

--- nv_net.py
+++ nv_net.py
@@ -46,8 +46,8 @@ for image, measurement in zip(images, measurements):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-# row, col, ch = 160, 320, 3  # Raw image format
-row, col, ch = 160 - top_crop - bot_crop, 320, 3  # Trimmed image format
+row, col, ch = 160, 320, 3  # Raw image format
+# row, col, ch = 160 - top_crop - bot_crop, 320, 3  # Trimmed image format
 print("row={}, col={}, ch={}".format(row,col,ch))
 
 model = Sequential()
