22b3866ce0d9c3ad556290135215243f2d0bf615
Shah Ismail <sim4life@yahoo.com>
2017-03-03 11:29:36 +0000

    lenet modified

--- lenet.py
+++ lenet.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -50,6 +50,7 @@ y_train = np.array(augmented_measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0)), input_shape=(160,320,3)))
 model.add(Convolution2D(6,5,5, activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5, activation="relu"))
