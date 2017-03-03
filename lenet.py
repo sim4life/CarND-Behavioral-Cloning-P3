e62af0d4e741f04ff56502a63ffdafed2889f35b
Shah Ismail <sim4life@yahoo.com>
2017-03-03 10:28:16 +0000

    lenet modified

--- lenet.py
+++ lenet.py
@@ -27,14 +27,21 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(image, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(Convolution2D(6,5,5, activation="relu"))
 model.add(MaxPooling2D())
-model.add(Convolution2D(16,5,5,activation="relu"))
+model.add(Convolution2D(16,5,5, activation="relu"))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
